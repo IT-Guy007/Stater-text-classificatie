b2cd3cb36f190369a8e2e3711513fd277c1d9f0d
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2023-06-05 22:23:25 +0200

    Adjusted code and documented about the model
    .py:
    - Removed scores, test complaint
    - Commented gridsearch
    - Adjusted model parameters
    - Updated imports, and variable csv_file
    
    .ipynb:
    - Added and adjusted documentation about the model and conclusion to advice how to improve the model.

--- development/models/multinomialLogisticRegression.py
+++ development/models/multinomialLogisticRegression.py
@@ -1,13 +1,11 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, classification_report
 from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import accuracy_score, classification_report
 
-csv_file = '../StaterData.csv'
+csv_file = 'StaterData.csv'
 dtypes = {
     'Date received': str,
     'Product': "category",
@@ -52,46 +50,30 @@ X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
 # Create multinomial logistic regression
-logregression = LogisticRegression(n_jobs=4, solver='saga', multi_class='multinomial', max_iter=1000, random_state=2)
+logregression = LogisticRegression(C=1, multi_class='multinomial', n_jobs=4, random_state=2, solver='saga')
 
 # Fit and predict
 logregression.fit(X_train_tfidf, y_train)
 y_pred = logregression.predict(X_test_tfidf)
 
-# Print scores
-print(f"score:{logregression.score(X_test_tfidf, y_test)}")
-print(f"accuracy:{accuracy_score(y_test, y_pred)}")
-print(f"f1 score:{f1_score(y_test, y_pred, average='weighted')}")
-
-cm = pd.DataFrame(confusion_matrix(y_test, y_pred), columns=cat_issue, index=cat_issue)
-cm = (cm.transpose()/cm.sum(axis=1)).transpose()
-
-sns.heatmap(cm, annot=True)
-plt.show()
-
 print(classification_report(y_test, y_pred, target_names=cat_issue))
 
-# Test the model with custom text
-new_complaint = "My mortgage was sold to Roundpoint company in XX/XX/2023. I got a letter in XXXX from my prior lender, XXXX XXXX stating this was to occur and the next payment to be made would go to Roundpoint. But this is false. The information in the letter is not true. I think it is a mistake of yours"
-new_complaint_vectorized = vectorizer.transform([new_complaint])
-issue_pred = logregression.predict(new_complaint_vectorized)
-print('Predicted issue:', issue_pred[0])
+# Use GridSearch to find the best parameters for the model
+# print("creating gridsearch")
+# param_grid = {
+#     'C': [0.001, 0.01, 0.1, 1, 10, 100],
+#     'solver': ['sag', 'saga'],
+#     'max_iter': [100, 1000, 2500, 5000],
+#     'penalty': ['l1', 'l2', 'elasticnet', None]
+# }
 
-print("creating gridsearch")
-param_grid = {
-    'C': [0.001, 0.01, 0.1, 1, 10, 100],
-    'solver': ['sag', 'saga'],
-    'max_iter': [100, 1000, 2500, 5000],
-    'penalty': ['l1', 'l2', 'elasticnet', None]
-}
+# # Subselect the first 10000 samples for the GridSearch
+# X_train_tfidf_subselect = X_train_tfidf[:10000]
+# y_train_subselect = y_train[:10000]
 
-# Subselect the first 10000 samples for the GridSearch
-X_train_tfidf_subselect = X_train_tfidf[:10000]
-y_train_subselect = y_train[:10000]
-
-model = LogisticRegression(multi_class='multinomial', n_jobs=4, random_state=2)
-grid = GridSearchCV(model, param_grid, verbose=3)
-grid.fit(X_train_tfidf_subselect, y_train_subselect)
-print(grid.best_params_)
-print(grid.best_estimator_)
-print(grid.best_score_)
+# model = LogisticRegression(multi_class='multinomial', n_jobs=4, random_state=2)
+# grid = GridSearchCV(model, param_grid, verbose=3)
+# grid.fit(X_train_tfidf_subselect, y_train_subselect)
+# print(grid.best_params_)
+# print(grid.best_estimator_)
+# print(grid.best_score_)
