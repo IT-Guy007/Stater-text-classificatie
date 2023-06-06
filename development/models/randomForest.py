75fdb24417f6331f6339a8464d38abf7ba06df53
larskroeze <s1147485@student.windesheim.nl>
2023-06-06 21:02:24 +0200

    name change + added python script

---
+++ development/models/randomForest.py
@@ -0,0 +1,196 @@
+# Various imports used for notebook
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import plot_tree
+import matplotlib.pyplot as plt
+import joblib
+from sklearn.metrics import f1_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Read cleaned dataset
+data = pd.read_sql_query("SELECT * FROM 'mortgage complaints'", "sqlite:///StaterData.db")
+
+# Drop dates for testing purposes
+# data.drop(['Date received', 'Date sent to company'], axis=1)
+
+# Limit test size due to performance issues
+data = data.loc[:1000]
+
+data[['Consumer complaint narrative', 'Issue']].head()
+
+# Define your features and target
+X = data['Consumer complaint narrative']  # text_column is the name of the column in your dataset that contains the text data
+y = data['Issue']  # target_column is the name of the column in your dataset that contains the target variable
+
+# Split your data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
+# Define params from GridSearch which performed the best
+param_grid = {
+    'n_estimators': 200,
+    'max_depth': None,
+    'min_samples_split': 2,
+    'min_samples_leaf': 1,
+}
+
+# Vectorize your text data using TF-IDF vectorizer
+vectorizer = TfidfVectorizer(stop_words='english')
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
+
+# Create a Random Forest model
+rf_model = RandomForestClassifier(random_state=2)
+
+# Set the params for the model
+rf_model.set_params(**param_grid)
+
+# Train the model on the training data
+rf_model.fit(X_train, y_train)
+
+# Predict the target variable for the testing data
+y_pred = rf_model.predict(X_test)
+
+# Evaluate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print('Accuracy:', accuracy)
+f1score = f1_score(y_test, y_pred, average='macro')
+print('F1 score:', f1score)
+
+custom_text = "My mortgage was sold to Roundpoint company in XX/XX/2023. I got a letter in XXXX from my prior lender, XXXX XXXX stating this was to occur and the next payment to be made would go to Roundpoint. X..."
+custom_text_bow = vectorizer.transform([custom_text])
+
+# Predict the issue for the custom text data
+predicted_issue = rf_model.predict(custom_text_bow)
+
+# Print the predicted issue
+print('Predicted issue:', predicted_issue)
+
+y_pred = rf_model.predict(X_test)
+
+# generate confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# create labels for xticklabels
+labels = data['Issue'].unique()
+print(labels)
+
+# plot confusion matrix as heatmap
+sns.heatmap(cm, annot=True, cmap="Blues", fmt="d", xticklabels=labels, yticklabels=labels)
+plt.xlabel("Predicted label")
+plt.ylabel("True label")
+plt.title("Confusion Matrix")
+plt.show()
+
+# Create a classification report
+report = classification_report(y_test, y_pred, output_dict=True)
+
+# Convert the report to a DataFrame
+df = pd.DataFrame(report).transpose()
+
+# Print the DataFrame
+print(df)
+
+# Visualize the first tree in the Random Forest
+plt.figure(figsize=(80, 80))
+
+plot_tree(rf_model.estimators_[0], feature_names=vectorizer.get_feature_names_out(), class_names=rf_model.classes_, filled=True, max_depth=1)
+plt.show()
+
+formatted_accuracy = str(int(accuracy*100))
+# joblib.dump(rf_model, 'rf_model ' + formatted_accuracy + '%.joblib')
+print(formatted_accuracy)
+
+# Load the saved model from the file
+# loaded_model = joblib.load('rf_model 70%.joblib')
+
+# Define your features and target
+X = data['Consumer complaint narrative']  # text_column is the name of the column in your dataset that contains the text data
+y = data['Issue']  # target_column is the name of the column in your dataset that contains the target variable
+
+# Split your data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
+# Vectorize your text data using a bag-of-words approach
+vectorizer = TfidfVectorizer(stop_words=['english'])
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
+
+# Create a Random Forest model
+rf_model = RandomForestClassifier(n_estimators=200, random_state=2)
+
+# Train the model on the training data
+rf_model.fit(X_train, y_train)
+
+# Get feature importances
+importances = rf_model.feature_importances_
+
+# Create a DataFrame with feature names and importances
+feature_importances = pd.DataFrame({'feature': vectorizer.get_feature_names(), 'importance': importances})
+
+# Sort the DataFrame by importance
+feature_importances = feature_importances.sort_values('importance', ascending=False)
+
+# Print the 10 most important features
+print(feature_importances.head(25))
+
+top_features = feature_importances.head(10)['feature'].values.tolist()
+
+# Define your features and target
+X = data['Consumer complaint narrative']  # text_column is the name of the column in your dataset that contains the text data
+y = data['Issue']  # target_column is the name of the column in your dataset that contains the target variable
+
+# Split your data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
+# Vectorize your text data using a bag-of-words approach
+vectorizer = TfidfVectorizer(stop_words='english', vocabulary=top_features)
+X_train = vectorizer.fit_transform(X_train)
+X_test = vectorizer.transform(X_test)
+
+# Create a Random Forest model
+rf_model = RandomForestClassifier(n_estimators=200, random_state=2)
+
+# Train the model on the training data
+rf_model.fit(X_train, y_train)
+
+# Predict the target variable for the testing data
+y_pred = rf_model.predict(X_test)
+
+# Evaluate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print('Accuracy:', accuracy)
+f1score = f1_score(y_test, y_pred, average='macro')
+print('F1 score:', f1score)
+
+# Define the parameter grid to search over
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'max_depth': [10, 20, 30, None],
+    'min_samples_split': [2, 5, 10],
+    'min_samples_leaf': [1, 2, 4],
+}
+
+# Create a Random Forest model
+rf_model = RandomForestClassifier(random_state=2)
+
+# Create a GridSearchCV object
+grid_search = GridSearchCV(
+    estimator=rf_model,
+    param_grid=param_grid,
+    cv=5,  # number of folds for cross-validation
+    scoring='accuracy',
+    # n_jobs=-1,  # use all available CPU cores
+)
+
+# Fit the grid search to the training data
+grid_search.fit(X_train, y_train)
+
+# Print the best hyperparameters and the corresponding F1 score
+print('Best hyperparameters:', grid_search.best_params_)
+print('Best accuracy score:', grid_search.best_score_)
