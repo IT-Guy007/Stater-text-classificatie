6cff2aaa2830f5caae03da10d81639267f555957
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2023-06-07 11:48:28 +0200

    Remove unnecessary import

--- development/models/multinomialLogisticRegression.py
+++ development/models/multinomialLogisticRegression.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 
 csv_file = 'StaterData.csv'
 dtypes = {
