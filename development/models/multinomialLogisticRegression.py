3f15f9ec589efaff9965454999be53aa64a58752
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2023-06-07 11:10:08 +0200

    Removed unnecessary code and adjusted csv_file folder direction

--- development/models/multinomialLogisticRegression.py
+++ development/models/multinomialLogisticRegression.py
@@ -3,7 +3,6 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import accuracy_score, classification_report
 
 csv_file = 'StaterData.csv'
 dtypes = {
@@ -30,19 +29,10 @@ dtypes = {
 
 # Read data from csv
 data = pd.read_csv(csv_file, dtype=dtypes)
-print(data.head())
-print(data.info())
 df = data[['Clean consumer complaint', 'Issue']].copy()
 
-# Set the independent and dependent variables as x and y
-x = df['Clean consumer complaint']
-y = df['Issue']
-
-# Get all categories of y for the results
-cat_issue = y.cat.categories
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=2)
+# Split the data into training and testing sets: x = complaint and y = issue
+X_train, X_test, y_train, y_test = train_test_split(df['Clean consumer complaint'], df['Issue'], test_size=0.3, random_state=2)
 
 # Vectorize the data with tfidf
 vectorizer = TfidfVectorizer(stop_words='english', token_pattern=r'\b[a-zA-Z]+\b')
@@ -56,24 +46,8 @@ logregression = LogisticRegression(C=1, multi_class='multinomial', n_jobs=4, ran
 logregression.fit(X_train_tfidf, y_train)
 y_pred = logregression.predict(X_test_tfidf)
 
-print(classification_report(y_test, y_pred, target_names=cat_issue))
-
-# Use GridSearch to find the best parameters for the model
-# print("creating gridsearch")
-# param_grid = {
-#     'C': [0.001, 0.01, 0.1, 1, 10, 100],
-#     'solver': ['sag', 'saga'],
-#     'max_iter': [100, 1000, 2500, 5000],
-#     'penalty': ['l1', 'l2', 'elasticnet', None]
-# }
-
-# # Subselect the first 10000 samples for the GridSearch
-# X_train_tfidf_subselect = X_train_tfidf[:10000]
-# y_train_subselect = y_train[:10000]
-
-# model = LogisticRegression(multi_class='multinomial', n_jobs=4, random_state=2)
-# grid = GridSearchCV(model, param_grid, verbose=3)
-# grid.fit(X_train_tfidf_subselect, y_train_subselect)
-# print(grid.best_params_)
-# print(grid.best_estimator_)
-# print(grid.best_score_)
+# function to classify a complaint
+def ask_question(text):
+    question_tfidf = vectorizer.transform([text])
+    prediction = logregression.predict(question_tfidf)
+    return prediction[0]
