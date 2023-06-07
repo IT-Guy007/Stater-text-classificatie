2f3479bcada34e6bee2134ba80e66d359ebfac1b
larskroeze <64012971+larskroeze@users.noreply.github.com>
2023-06-07 10:52:55 +0200

    changed on review
    read from csv cleaned dataset instead of database. Removed prints on the py script. removed accuracy predidtion on python script.

--- development/models/randomForest.py
+++ development/models/randomForest.py
@@ -13,14 +13,11 @@ from sklearn.metrics import f1_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-# Read cleaned dataset
-data = pd.read_sql_query("SELECT * FROM 'mortgage complaints'", "sqlite:///StaterData.db")
-
-# Limit test size due to performance issues
-data = data.loc[:1000]
+# Read cleaned dataset created from complaintCleaner.py
+data = pd.read_csv('StaterData.csv')
 
 # Define your features and target
-X = data['Consumer complaint narrative']  # text_column is the name of the column in your dataset that contains the text data
+X = data['Clean consumer complaint']  # text_column is the name of the column in your dataset that contains the text data
 y = data['Issue']  # target_column is the name of the column in your dataset that contains the target variable
 
 # Split your data into training and testing sets
@@ -28,10 +25,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 # Define params from GridSearch which performed the best
 param_grid = {
-    'n_estimators': 200,
-    'max_depth': None,
-    'min_samples_split': 2,
-    'min_samples_leaf': 1,
+    'n_estimators': 200
 }
 
 # Vectorize your text data using TF-IDF vectorizer
@@ -51,20 +45,11 @@ rf_model.fit(X_train, y_train)
 # Predict the target variable for the testing data
 y_pred = rf_model.predict(X_test)
 
-# Evaluate the accuracy of the model
-accuracy = accuracy_score(y_test, y_pred)
-print('Accuracy:', accuracy)
-f1score = f1_score(y_test, y_pred, average='macro')
-print('F1 score:', f1score)
-
 custom_text = "My mortgage was sold to Roundpoint company in XX/XX/2023. I got a letter in XXXX from my prior lender, XXXX XXXX stating this was to occur and the next payment to be made would go to Roundpoint. X..."
 custom_text_bow = vectorizer.transform([custom_text])
 
 # Predict the issue for the custom text data
 predicted_issue = rf_model.predict(custom_text_bow)
-
-# Print the predicted issue
-print('Predicted issue:', predicted_issue)
 
 y_pred = rf_model.predict(X_test)
 
@@ -73,22 +58,6 @@ report = classification_report(y_test, y_pred, output_dict=True)
 
 # Convert the report to a DataFrame
 df = pd.DataFrame(report).transpose()
-
-# Print the DataFrame
-print(df)
-
-# Visualize the first tree in the Random Forest
-plt.figure(figsize=(80, 80))
-
-plot_tree(rf_model.estimators_[0], feature_names=vectorizer.get_feature_names_out(), class_names=rf_model.classes_, filled=True, max_depth=1)
-plt.show()
-
-formatted_accuracy = str(int(accuracy*100))
-# joblib.dump(rf_model, 'rf_model ' + formatted_accuracy + '%.joblib')
-print(formatted_accuracy)
-
-# Load the saved model from the file
-# loaded_model = joblib.load('rf_model 70%.joblib')
 
 # Define your features and target
 X = data['Consumer complaint narrative']  # text_column is the name of the column in your dataset that contains the text data
@@ -117,9 +86,6 @@ feature_importances = pd.DataFrame({'feature': vectorizer.get_feature_names(), '
 # Sort the DataFrame by importance
 feature_importances = feature_importances.sort_values('importance', ascending=False)
 
-# Print the 10 most important features
-print(feature_importances.head(25))
-
 top_features = feature_importances.head(10)['feature'].values.tolist()
 
 # Define your features and target
@@ -145,9 +111,7 @@ y_pred = rf_model.predict(X_test)
 
 # Evaluate the accuracy of the model
 accuracy = accuracy_score(y_test, y_pred)
-print('Accuracy:', accuracy)
 f1score = f1_score(y_test, y_pred, average='macro')
-print('F1 score:', f1score)
 
 # # Define the parameter grid to search over
 # param_grid = {
